2cc819a8c7988a3233f3058e2da0574fc0870076
CyanFrost <patricknopely@gmail.com>
2015-03-02 07:52:24 -0600

    Add Main
    Added the main class.

---
+++ src/com/dayellecraft/mc1_8/plugin/daysentials/main/Main.java
@@ -0,0 +1,18 @@
+package com.dayellecraft.mc1_8.plugin.daysentials.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override public void onEnable() {
+		
+	}
+	
+	@Override public void onDisable() {
+		
+	}
+	
+	@Override public void onLoad() {
+		
+	}
+}
